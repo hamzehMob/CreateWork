740926bfb7c79f457e102cacaeb1ca36d9d708e8
hamzehMob <hamzeh.mobiles22@gmail.com>
2025-02-02 21:14:48 +0300

    CreateWork

--- script.js
+++ script.js
@@ -9,8 +9,12 @@ const app = initializeApp(appSettings);
 const database = getDatabase(app);
 const users = ref(database, "Users");
 
+const inputfieldUsername = document.getElementById("signup-username");
+const inputfieldDate = document.getElementById("signup-date");
+const inputfieldLocation = document.getElementById("signup-location");
 const inputfieldEmail = document.getElementById("signup-email");
 const inputfieldPassword = document.getElementById("signup-password");
+const inputfieldLevel = document.getElementById("signup-level");
 const button = document.getElementById("signup-button");
 
 const panelLogin = document.getElementById("login");
@@ -20,11 +24,15 @@ panelLogined.style.display = "none";
 button.addEventListener("click", function()
 {
   let user = {
+    username: inputfieldUsername.value,
     email: inputfieldEmail.value,
-    password: inputfieldPassword.value
+    password: inputfieldPassword.value,
+    date: inputfieldDate.value,
+    location: inputfieldLocation.value,
+    level: inputfieldLevel.value
   };
 
-  if(user.email.length > 0 && user.password.length > 0)
+  if(user.username.length > 0 && user.email.length > 0 && user.password.length > 0 && user.date.length > 0 && user.location.length > 0 && user.level.length > 0)
   {
     push(users, user);
     panelLogin.style.display = "none";
